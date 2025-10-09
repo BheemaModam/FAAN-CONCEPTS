a42dc400c8fa25444e9fa0b5cb7d66a07de27026
BheemaModam <modambheema@gmail.com>
2025-10-09 19:23:51 +0530

    Arrays-started

---
+++ Arrays/dup2.js
@@ -0,0 +1,36 @@
+/*
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k. 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+*/ 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let hash=new Set();
+    for(let i=0;i<nums.length;i++){
+        if(hash.has(nums[i])) return true;
+        hash.add(nums[i]);
+        // size overflow
+        if(hash.size>k){
+            hash.delete(nums[i-k])
+        }
+    }
+    return false    
+};
+const result = containsNearbyDuplicate([1,2,3,1], 3);
+console.log(result);
---
+++ Arrays/duplicates.js
@@ -0,0 +1,47 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+// 1.using SETS 
+    return new Set(nums).size !== nums.length
+// 2. using hasmap if we found element in hashmap we return true
+// 3
+};
+
+const result=containsDuplicate([1,1,1,3,3,4,3,2,4,2]);
+console.log(result);
