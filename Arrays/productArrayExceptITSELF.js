eb38d9fb5ffa9b7be668d1f0bffb5d54c99778d0
BheemaModam <modambheema@gmail.com>
2025-10-16 00:20:24 +0530

    produtExceptITSELF

---
+++ Arrays/productArrayExceptITSELF.js
@@ -0,0 +1,54 @@
+/*
+todo: 238. Product of Array Except Self
+Medium
+Topics
+
+Hint
+Given an integer array nums, return an array answer such that answer[i] is 
+equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation. 
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  //todo: PRE FIX AND POST FIX APPROACH
+  let prefix = 1;
+  let postfix = 1;
+  let result = new Array(nums.length).fill(1);
+  // CALUCULATE PREFIX
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix = prefix * nums[i];
+  }
+  // CALUCULATE PostFIX
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] = result[i] * postfix;
+    postfix = postfix * nums[i];
+  }
+  return result;
+};
+
+const res1 = productExceptSelf([1, 2, 3, 4]);
+console.log(res1);
+
+const res2 = productExceptSelf([-1, 1, 0, -3, 3]);
+console.log(res2);
