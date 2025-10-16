174fe4c8ed66c55802d0220cd95613493d0e5e47
BheemaModam <modambheema@gmail.com>
2025-10-16 22:59:14 +0530

    created-practise-branch

--- Arrays/topKelement.js
+++ Arrays/topKelement.js
@@ -36,7 +36,16 @@ where n is the array's size.
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function (nums, k) {};
+var topKFrequent = function (nums, k) {
+    // 1.create hashMap
+    let hash=new Map();
+    for(let i=0;i<=nums.length;i++){
+        hash.set(nums[i],(hash.get(nums[i])||0)+1)
+    }
+    
+    // 2.create minHeap with size K
+
+};
 
 const res1 = topKFrequent([1, 1, 1, 2, 2, 3], 2);
 console.log(res1);
