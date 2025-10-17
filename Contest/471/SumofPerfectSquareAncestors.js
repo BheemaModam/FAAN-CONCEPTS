c0e41a9d159c93d326f40d93038e84f35a26c4f9
BheemaModam <modambheema@gmail.com>
2025-10-17 23:56:43 +0530

    contest-471-lastQ

---
+++ Contest/471/SumofPerfectSquareAncestors.js
@@ -0,0 +1,30 @@
+/*
+todo: 3715. Sum of Perfect Square Ancestors
+Hard
+Topics
+premium lock iconCompanies
+Hint
+
+You are given an integer n and an undirected tree rooted at node 0 with n nodes 
+numbered from 0 to n - 1. This is represented by a 2D array edges of length n - 1, 
+where edges[i] = [ui, vi] indicates an undirected edge between nodes ui and vi.
+
+You are also given an integer array nums, where nums[i] is the positive integer assigned to node i.
+Define a value ti as the number of ancestors of node i such that the 
+product nums[i] * nums[ancestor] is a.
+
+Return the sum of all ti values for all nodes i in range [1, n - 1].
+
+Note:
+
+    In a rooted tree, the ancestors of node i are all nodes on the pa
+*/
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfAncestors = function(n, edges, nums) {
+    
+};
