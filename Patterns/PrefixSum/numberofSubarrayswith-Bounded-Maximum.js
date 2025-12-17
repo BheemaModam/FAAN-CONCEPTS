c43986f20d9258988a5b7de0b6ee317390c319a2
BheemaModam <modambheema@gmail.com>
2025-12-17 22:17:25 +0530

    prefix&suffix-starter-file

--- Patterns/PrefixSum/numberofSubarrayswith-Bounded-Maximum.js
+++ Patterns/PrefixSum/numberofSubarrayswith-Bounded-Maximum.js
@@ -39,5 +39,13 @@ Constraints:
  * @return {number}
  */
 var numSubarrayBoundedMax = function(nums, left, right) {
-    
+    let ans = 0, low = 0, mid = 0
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i]
+        if (num > right) mid = 0
+        else ans += ++mid
+        if (num >= left) low = 0
+        else ans -= ++low
+    }
+    return ans
 };
