ce133b10894e390ebfdd38e029afcddfcff4d7f9
BheemaModam <modambheema@gmail.com>
2025-12-26 11:45:51 +0530

    2sum

--- Arrays/2Sum.js
+++ Arrays/2Sum.js
@@ -1,7 +1,9 @@
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
 
 You can return the answer in any order.
 
@@ -29,7 +31,30 @@ Output: [0,1]
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+    // 1.Hashmaps not working????
+    // let hash = new Map();
+    // for (let i = 0; i < nums.length; i++) {
+        
+    //     if (!hash.get(target - nums[i])) {
+    //         hash.set(target - nums[i], i);
+            
+    //     }else{
+    //         return [i, hash.get(target - nums[i])];            
+    //     }
+    // }
+    // return []
 
-const result = twoSum([2, 7, 11, 15], 9);
+    // 2. using OBJECTS--->working fine.
+    let findPair={};
+    for(let i=0;i<nums.length;i++){
+        if(target-nums[i] in findPair){
+            return [i,findPair[target-nums[i]]]
+        }else{
+            findPair[nums[i]]=i
+        }
+    }
+};
+
+const result = twoSum([2,7,11,15], 9);
 console.log(result);
