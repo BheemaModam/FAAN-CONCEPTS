5eb51def139bf2885019753e1549fceaf2748cd9
it4yourbusinessBheemaIT4 <modambheema@gmail.com>
2026-01-30 14:23:29 +0530

    2-pointers-in-merge-patterns

---
+++ Patterns/MergeIntervals/insert-interval.js
@@ -0,0 +1,72 @@
+/*
+TODO: 57. Insert Interval
+Medium
+
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Constraints:
+    0 <= intervals.length <= 104
+    intervals[i].length == 2
+    0 <= starti <= endi <= 105
+    intervals is sorted by starti in ascending order.
+    newInterval.length == 2
+    0 <= start <= end <= 105
+
+
+*/
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (intervals, newInterval) {
+
+  // interval is not insertable
+  intervals.push(newInterval)
+  const mergeInterval=function(intervals){
+    intervals.sort((a,b)=>a[0]-b[0])
+    let result=[intervals[0]];
+    for(let i=1;i<intervals.length;i++){
+      let e1=result[result.length-1][1];
+      let s2=intervals[i][0];
+      let e2=intervals[i][1];
+      if(e1>=s2){
+        result[result.length-1][1]=Math.max(e1,e2)
+      }else{
+        result.push(intervals[[i]])
+      }
+    }
+    return result;
+
+  };
+  return mergeInterval(intervals);
+
+
+};
+
+const res = insert(
+  [
+    [1, 2],
+    [3, 5],
+    [6, 7],
+    [8, 10],
+    [12, 16],
+  ],
+  [4, 8]
+);
+console.log("Result->", res);
